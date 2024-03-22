d7294e8771e0d3a531c2c9a5474ddfd47afe11ff
Your Name <you@example.com>
2024-03-22 03:40:31 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -76,23 +76,18 @@ posiciona en el elemento anterior.
 */
 
 void eliminaElementos(int** L, int elem) {
-    // Eliminar los elementos iguales a elem al inicio de la lista
-    while (*L != NULL && **L == elem) {
-        int* temp = *L;
-        *L = (*L)[1];
-        free(temp);
+    while (**L != elem && **L != 0) {
+        *L = *L + 1;
     }
 
-    // Recorrer el resto de la lista para eliminar los elementos iguales a elem
+    if (**L == elem) {
+        *L = *L + 1;
+    }
+
     int* currentPtr = *L;
-    while (currentPtr != NULL && currentPtr[1] != NULL) {
-        if (currentPtr[1] == elem) {
-            int* temp = currentPtr[1];
-            currentPtr[1] = temp[1];
-            free(temp);
-        } else {
-            currentPtr = currentPtr[1];
-        }
+    while (*currentPtr != 0 && currentPtr[1] != 0) {
+        *currentPtr = currentPtr[1];
+        currentPtr = &currentPtr[1];
     }
 }
 
